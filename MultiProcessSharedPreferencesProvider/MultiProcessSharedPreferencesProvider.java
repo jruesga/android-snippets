e8a182e991d2cf9eec6dc039702449acc65eda61
Jorge Ruesga <jorge@ruesga.com>
2016-02-09 03:14:54 +0100

    Add a package definition to MultiProcessSharedPreferencesProvider class
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- MultiProcessSharedPreferencesProvider/MultiProcessSharedPreferencesProvider.java
+++ MultiProcessSharedPreferencesProvider/MultiProcessSharedPreferencesProvider.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.ruesga.preferences;
+
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
